6e761a8f94ff8fd07381047a002819a7f31db2ce
tekilit@gmail.com <67770867+tekilit@users.noreply.github.com>
2020-10-09 12:15:01 +0200

    repo+ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,21 @@
+var temperature = 20
+var sky = 'lloviendo'
+var message= "Ponte"
+var outfit = message  
+
+
+if (temperature >25) {
+  console.log (outfit + ' una camiseta' )
+}
+
+else if (temperature <= 25) {
+  console.log (outfit + ' un sweater')
+} 
+
+if  (sky === 'soleado') {
+  console.log ('y un sombrero')
+}
+
+ if (sky === 'lloviendo') {
+  console.log ('y lleva un paraguas')
+}
